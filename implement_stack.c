f1f0477b8b30ecc88a11880dde35e92abb5706cd
Biswajit <biswajitjena529@gmail.com>
2024-11-22 16:10:47 +0530

    dsa practice

---
+++ implement_stack.c
@@ -0,0 +1,16 @@
+// impliment stack using linked list .
+// Stack is a linear data structure that follows the Last-In-First-Out (LIFO) order of operations.
+// create stack
+
+#include "stdio.h"
+#include "stdlib.h"
+
+// sturcture to create stack 
+
+struct stack
+{
+    int * top;
+    int * stack_arr;
+    int size;
+
+}stack_p;
